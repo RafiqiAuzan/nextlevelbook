0c78b235292dd7443bb8594d876e4dc0da4e6cab
RafiqiAuzan <rafikids32@gmail.com>
2023-11-19 21:39:51 +0700

    install tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,13 @@
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
 </head>
 <body>
-    
-
-@viteReactRefresh
-@vite('resources/js/app.js')
+  <h1 class="text-3xl font-bold underline">
+    Hello worldddaselole
+  </h1>
 </body>
 </html>
