1a6f3a564447a021188fa9c54beedb50c332bfaf
RafiqiAuzan <rafikids32@gmail.com>
2023-11-24 17:14:28 +0700

    fixed 1 error line at applicationlogo.jsx

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -3,5 +3,3 @@ export default function ApplicationLogo(props) {
         <img src="img/Logo/logo.png" alt="SVG Image" id="your-svg-image" className=" w-44" />
     );
 }
-
-w-4 h-4 text-gray-500 dark:text-gray-400
