967ad8aa859ab93f1d387cce0a3d4b22e53f29fd
RafiqiAuzan <rafikids32@gmail.com>
2023-11-24 22:54:25 +0700

    updated navbar and added card

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({ user }) => {
                     <div className="dropdown dropdown-end ml-[130px]">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                            <img alt="Tailwind CSS Navbar component" src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            <img alt="Tailwind CSS Navbar component" src="img/landing/profil.jpeg" />
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
